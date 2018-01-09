67844902fad76a57982f85b7a659db34c8dc08a3
yennyelizabeth <yenny.elizabeth@hotmail.com>
2018-01-09 16:48:41 -0500

    añadiendo data de ejemplo de carga y funcionalidad de extraer

--- js/list-service.js
+++ js/list-service.js
@@ -4,4 +4,23 @@ $( document ).ready(function() {
       $(".nav").find(".activa").removeClass("activa");
       $(this).addClass("activa");
     });
+
+    /* declaración de variables */
+    /*var url = dataPeople[0,3];*/
+    var url = dataPeople[0,3];
+   
+    var $idName = $('#name');
+    var $idResum = $('#resum');
+
+    /* extraer data */
+    $idName.text( dataPeople[0,1] );
+    $idResum.text( dataPeople[0,5] );
+      $("#imagen").attr('src', url);
+      $("#imagen").attr('alt', 'Imagen no disponible');
+      $("#imagen").attr('title', 'Imagen de carga');
+
+
+
+
   });
+
