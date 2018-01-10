5cac3fcceab2e5aa4becfb6df04a71f5be53b74d
yennyelizabeth <yenny.elizabeth@hotmail.com>
2018-01-09 20:23:08 -0500

    actualizando data

--- js/data.js
+++ js/data.js
@@ -1,16 +1,8 @@
-
-/*
-dataPeople = [{id:'O00001' ,name:"Miguel Quiñones",year:"40",oficio: "electricista",
-               photo:'../assets/images/list-service/electricista-1.jpg',
-               password:'MQ201801',
-               email:'miguel-quinones@gmail.com',
-               resum: "Electricista con 10 años en experiencia, puntual, responsable y calidad de trabajo"
-              } ];*/
-
 dataPeople = [];
 portafolio = [];
+providers = [];
 
-/************************************* Persona Nro 1 *****************************************/
+/* Persona Nro 1 */
 dataPeople [0,0] = 'O00001'; /* correlative-identificador */
 dataPeople [0,1] = 'Miguel Quiñones'; /* name */
 dataPeople [0,2] = '40'; /* edad */
@@ -21,6 +13,7 @@ dataPeople [0,6] = 'electricista'; /* oficio */
 dataPeople [0,7] = '6'; /* nro de recomendaciones */
 dataPeople [0,8] = '40'; /* nro de likes */
 dataPeople [0,9] = '2'; /* nro de oficios */
+dataPeople [0,10] = '3'; /* nro de calificaciones */
 
 portafolio [0,0] = 'O00001'; /* correlative-identificador */
 portafolio [0,1] = '../assets/images/list-service/port-1-1.jpg'; /* trabajo 1  */
@@ -28,8 +21,8 @@ portafolio [0,2] = '../assets/images/list-service/port-1-2.jpg'; /* trabajo 2  *
 portafolio [0,3] = '../assets/images/list-service/port-1-3.jpg'; /* trabajo 3  */
 
 
-/************************************* Persona Nro 2 *****************************************/
-/*__Datos Personales__*/
+/* Persona Nro 2 */
+/* Datos Personales */
 dataPeople [1,0] = 'O00002'; /* correlative-identificador */
 dataPeople [1,1] = 'Marcos Inca'; /* name */
 dataPeople [1,2] = '45'; /* edad */
@@ -40,9 +33,42 @@ dataPeople [1,6] = 'electricista'; /* oficio */
 dataPeople [1,7] = '26'; /* nro de recomendaciones */
 dataPeople [1,8] = '25'; /* nro de likes */
 dataPeople [1,9] = '3'; /* nro de oficios */
+dataPeople [1,10] = '4'; /* nro de calificaciones */
 
-/*__ Fotos de Portafolio__*/
+/* Fotos de Portafolio */
 portafolio [1,0] = 'O00002'; /* correlative-identificador */
 portafolio [1,1] = '../assets/images/list-service/port-2-1.jpg'; /* trabajo 1  */
 portafolio [1,2] = '../assets/images/list-service/port-2-2.jpg'; /* trabajo 2  */
-portafolio [1,3] = '../assets/images/list-service/port-1-3.jpg'; /* trabajo 3  */
+portafolio [1,3] = '../assets/images/list-service/port-2-3.jpg'; /* trabajo 3  */
+
+/* Persona Nro 3 */
+/* Datos Personales */
+dataPeople [2,0] = 'O00002'; /* correlative-identificador */
+dataPeople [2,1] = 'Marcos Inca'; /* name */
+dataPeople [2,2] = '45'; /* edad */
+dataPeople [2,3] = '../assets/images/list-service/electricista-2.jpg'; /* foto */
+dataPeople [2,4] = 'marcos-inca@gmail.com'; /* email */
+dataPeople [2,5] = 'Electricista con 15 años en experiencia y calidad en el trabajo'; /* resumen */
+dataPeople [2,6] = 'electricista'; /* oficio */
+dataPeople [2,7] = '26'; /* nro de recomendaciones */
+dataPeople [2,8] = '25'; /* nro de likes */
+dataPeople [2,9] = '3'; /* nro de oficios */
+dataPeople [2,10] = '4'; /* nro de calificaciones */
+
+/*__ Fotos de Portafolio__*/
+portafolio [2,0] = 'O00002'; /* correlative-identificador */
+portafolio [2,1] = '../assets/images/list-service/port-3-1.jpg'; /* trabajo 1  */
+portafolio [2,2] = '../assets/images/list-service/port-3-2.jpg'; /* trabajo 2  */
+portafolio [2,3] = '../assets/images/list-service/port-3-3.jpg'; /* trabajo 3  */
+
+
+/* Proveedor Nro 1 */
+providers [0,0] = 'P00001'; /* correlative-identificador */
+providers [0,1] = 'Maestro'; /* name */
+providers [0,2] = 'www.'; /* edad */
+
+
+products [0,0] = 'O00001'; /* correlative-identificador */
+products [0,1] = '../assets/images/list-service/port-1-1.jpg'; /* trabajo 1  */
+products [0,2] = '../assets/images/list-service/port-1-2.jpg'; /* trabajo 2  */
+products [0,3] = '../assets/images/list-service/port-1-3.jpg'; /* trabajo 3  */
