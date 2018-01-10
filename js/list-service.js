73ae519c542df111c2d43603d6f08de1515896f9
yennyelizabeth <yenny.elizabeth@hotmail.com>
2018-01-10 17:07:47 -0500

    actualizando identado

--- js/list-service.js
+++ js/list-service.js
@@ -1,9 +1,8 @@
-
 /* funcionalidad */
-$( document ).ready(function() {
-  $(".nav li").on("click", function(){
-    $(".nav").find(".activa").removeClass("activa");
-    $(this).addClass("activa");
+$(document).ready(function() {
+  $('.nav li').on('click', function() {
+    $('.nav').find('.activa').removeClass('activa');
+    $(this).addClass('activa');
   });
   /*
   var url = dataPeople[0,3];
@@ -19,64 +18,53 @@ $( document ).ready(function() {
   $('#imagen').attr('alt', 'Imagen no disponible');
   $('#imagen').attr('title', 'Imagen de carga');
 */
-
   /* publicaciones */
   var btn = $('#btn');
-	// btn.prop("disabled", true);
-	var textarea = $('#textarea');
-	
-	textarea.keyup(function(e){
-		validation();
-	});
-	
-	function validation(){
-		var long = textarea.val().length;
-		var onlyText = textarea.val().replace(/\s/g,'');
-		if (long == 0 || onlyText == 0) {
-			// btn.removeAttr('disabled');
+  /* btn.prop("disabled", true);*/
+  var textarea = $('#textarea');
+  textarea.keyup(function(event) {
+    validation();
+  });
+
+  function validation() {
+    var long = textarea.val().length;
+	  var onlyText = textarea.val().replace(/\s/g , '');
+	  if (long === 0 || onlyText === 0){
+	  }
+	  else {
 		}
-		else {
-			// btn.prop("disabled", true);
-		}
-  }
-  
-	btn.click(function() {
-		addTask();
-		textarea.val('');
-		//  btn.prop("disabled", true);
-	});
-	function addTask() {
-		var taskContent = textarea.val();
-		var containerToDo = $('#container-to-do');
+   }
+
+  btn.click(function() {
+    addTask();
+		 textarea.val('');
+	 });
+	 function addTask() {
+	 	var taskContent = textarea.val();
+	  var containerToDo = $('#container-to-do');
     var newTask = $('<div>');
     var label = $('<label>');
-		/*var check = $('<input>');*/
     var text = $(document.createTextNode(taskContent));
     var icon = $('<i>');
 
-		newTask.addClass('new');
-		newTask.addClass('checkbox');
-		containerToDo.append(newTask);
-	/*	check.attr('type','checkbox');*/
-/*		label.append(check);*/
-		newTask.append(label);	
-		label.append(text);	
-		icon.addClass('glyphicon glyphicon-point icon');
-		newTask.append(icon);    
-    textarea.val('')
+		  newTask.addClass('new');
+		  newTask.addClass('checkbox');
+		  containerToDo.append(newTask);
+		  newTask.append(label);
+		  label.append(text);
+		  icon.addClass('glyphicon glyphicon-point icon');
+	  	newTask.append(icon);
+    textarea.val('');
 
-		/*check.click(function() {
-			labeledTask();
-		});*/
-		function labeledTask(){
-			label.toggleClass('labeled');
-		}
-		icon.click( function() {
-			deleteTask();
+	  function labeledTask(){
+	     label.toggleClass('labeled');
+		  }
+	  	icon.click(function() {
+		 	deleteTask();
     });
-    
+
 		function deleteTask(){
-			newTask.remove()
+			 newTask.remove();
 		}
 	}
 });
