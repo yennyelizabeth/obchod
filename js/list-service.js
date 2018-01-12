3a234190ade8db749050d85c602c115f388bcbb4
yennyelizabeth <yenny.elizabeth@hotmail.com>
2018-01-12 02:40:15 -0500

    mejorando identacion

--- js/list-service.js
+++ js/list-service.js
@@ -19,63 +19,61 @@ $(document).ready(function() {
   $('#imagen').attr('title', 'Imagen de carga');
 */
 
-  /* publicaciones */
+  /* Funcionalidad - Sección Publicaciones */
   var btn = $('#btn');
-  //   btn.prop("disabled", true);
   var textarea = $('#textarea');
 
-	textarea.keyup(function(e){
-		validation();
-	});
-
-	function validation(){
-		var long = textarea.val().length;
-		var onlyText = textarea.val().replace(/\s/g,'');
-		if (long == 0 || onlyText == 0) {
-			// btn.removeAttr('disabled');
-		}
-		else {
-			// btn.prop("disabled", true);
-		}
+  btn.prop('disabled', true);
+	 textarea.keyup(function(event) {
+	    validation();
+	 });
+  /* función valida publicación */
+	 function validation() {
+	 	var long = textarea.val().length;
+	 	var onlyText = textarea.val().replace(/\s/g, '');
+		 if (long === 0 || onlyText === 0) {
+       		 btn.prop('disabled', true) ;
+	   } else {
+	  	 btn.removeAttr('disabled', false);
+	   }
   }
+  /* evento click */
+	 btn.click(function() {
+	  addPublication();
+	 	textarea.val('');
+	  btn.prop('disabled', true);
+	 });
+  /* función publicar */
+	 function addPublication() {
+    /* declaración de variables */
+	 	var publicationContent = textarea.val();
+	 	var $containerToDo = $('#container-to-do');
+    var $newPublication = $('<div>');
+    var $label = $('<label>');
+    var $text = $(document.createTextNode(publicationContent));
+    var $icon = $('<i>');
+    /* adición de clases y asignación de padres e hijos*/
+		 $newPublication.addClass('new');
+		 $newPublication.addClass('checkbox');
+	   $containerToDo.append($newPublication);
+    $newPublication.append($label);
+		 $label.append($text);
+	 	$icon.addClass('glyphicon glyphicon-point icon');
+	 	$newPublication.append($icon);
+    textarea.val('');
+    btn.prop('disabled', true);
 
-	btn.click(function() {
-		addTask();
-		textarea.val('');
-		//  btn.prop("disabled", true);
-	});
-	function addTask() {
-		var taskContent = textarea.val();
-		var containerToDo = $('#container-to-do');
-    var newTask = $('<div>');
-    var label = $('<label>');
-		/*var check = $('<input>');*/
-    var text = $(document.createTextNode(taskContent));
-    var icon = $('<i>');
+    /* función publicar */
+	 	function labeledTask() {
+			 label.toggleClass('labeled');
+		 }
 
-		newTask.addClass('new');
-		newTask.addClass('checkbox');
-		containerToDo.append(newTask);
-	/*	check.attr('type','checkbox');*/
-/*		label.append(check);*/
-		newTask.append(label);
-		label.append(text);
-		icon.addClass('glyphicon glyphicon-point icon');
-		newTask.append(icon);
-    textarea.val('')
-
-		/*check.click(function() {
-			labeledTask();
-		});*/
-		function labeledTask(){
-			label.toggleClass('labeled');
-		}
-		icon.click( function() {
-			deleteTask();
+	 	icon.click(function() {
+		 	deleteTask();
     });
 
-		function deleteTask(){
-			newTask.remove()
-		}
-	}
+		 function deleteTask() {
+		 	$newPublication.remove();
+		 }
+	 }
 });
